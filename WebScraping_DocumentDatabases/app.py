5c59820661654a53e6ea35b0a6968415ae9c15f2
Ken1209 <kentorres822@gmail.com>
2019-05-03 13:54:58 -0500

    1st ETL mini project

--- WebScraping_DocumentDatabases/app.py
+++ WebScraping_DocumentDatabases/app.py
@@ -1,27 +1,43 @@
+
+
+
 import scrape_costa
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from pymongo import ReplaceOne
+import pymongo
 
+# First Database Connection
 app = Flask(__name__)
-
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/temp_dict'
 mongo = PyMongo(app)
 
+# 2nd Database Connection
+mongo2 = PyMongo(app,uri='mongodb://localhost:27017/craig_dict')
 
 @app.route('/')
 def home():
     temp_dict = mongo.db.temp_dict.find_one()
-    
-    return render_template('index.html',dict=temp_dict)
+    craig_db = mongo2.db.craig.find()
+    return render_template('index.html',dict=temp_dict,
+                            craig_dict = craig_db)
 
 @app.route('/scrape')
 def scrape():
     temp_dict = mongo.db.temp_dict
-
-    dict_data = scrape_costa.scrape_info()
-
+    dict_data, craig_db = scrape_costa.scrape_info()
     temp_dict.update({}, dict_data,upsert=True)
 
+    # 2nd Database Connection
+    mongo2.db.craig.bulk_write([
+    ReplaceOne(
+        { "title": m['title']},
+         m,
+         upsert=True
+         )
+         for m in craig_db
+         ])
+    
     return redirect("/",code=302)
 
 if __name__ == "__main__":
--- WebScraping_DocumentDatabases/scrape_costa.py
+++ WebScraping_DocumentDatabases/scrape_costa.py
@@ -9,11 +9,27 @@ def scrape_info():
 
     soup = bs(response.text,'html.parser')
 
+    # Link for Image (still needs work)
+    for link in soup.find_all('img',class_='img-fluid animals'):
+        img = link['src']
+        break
+    # Image
+    
     for val in soup.find_all('div',id='weather'):
         temp_dict = {
             'max':val.find_all('strong')[1].text,
             'min':val.find_all('strong')[0].text
         }
 
-    return temp_dict
+
+    url = 'https://houston.craigslist.org/search/pet?s=100'
+    response = requests.get(url)
+
+    soup = bs(response.text,'html.parser')
+    craig_dict = []
+    for val in soup.find_all('p',class_='result-info'):
+        craig = {'title':val.a.text,'link':val.a['href']}
+        craig_dict.append(craig)
+
+    return temp_dict, craig_dict
     
